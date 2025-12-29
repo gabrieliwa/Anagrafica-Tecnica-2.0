37b61b3cc207c871d32875b8289050c5478a9d23
gabrieliwa <spades_smugger.0e@icloud.com>
2025-12-29 17:03:13 +0100

    New app with correct name

---
+++ anagrafica tecnica app/anagrafica tecnica app/anagrafica_tecnica_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  anagrafica_tecnica_appApp.swift
+//  anagrafica tecnica app
+//
+//  Created by Gabriele Giardino on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct anagrafica_tecnica_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
