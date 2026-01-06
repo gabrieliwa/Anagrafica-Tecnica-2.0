ea03de5ef7aca00e5a52468d6cd8d9816b1676b2
gabrieliwa <spades_smugger.0e@icloud.com>
2026-01-06 10:30:34 +0100

    chatgpt app update

---
+++ anagrafica tecnica app/AnagraficaTecnica/AnagraficaTecnica/AnagraficaTecnicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnagraficaTecnicaApp.swift
+//  AnagraficaTecnica
+//
+//  Created by Gabriele Giardino on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AnagraficaTecnicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
